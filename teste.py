e8ee5ba1be4e56916696607463f062246b9462d2
Douglas Lopes Gonçalves <douglasfun@gmail.com>
2017-09-06 15:11:53 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,14 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program starts on " + time.ctime())
+
+while (break_count < total_breaks):
+    time.sleep(5)
+    webbrowser.open("http://serpro.gov.br")
+    break_count = break_count + 1
+
+
